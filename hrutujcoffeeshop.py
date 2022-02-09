7f542fd3e355c4d669cd3721d10770d53b249557
hrutuj1321 <92776723+hrutuj1321@users.noreply.github.com>
2022-02-09 19:29:48 +0530

    Add files via upload

---
+++ hrutujcoffeeshop.py
@@ -0,0 +1,64 @@
+#Hrutuj's Test project.</>
+
+#going to build a coffee shop:)
+
+#making Robot parker!!
+
+print("Hello, welcome to hrutuj coffee!!!")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + ", thank you so much for coming in today,\n")
+
+menu = "black coffee, Espresso, latte, cappucino"
+
+print(name + ", What would you like from our menu today? Here is what we are serving.\n" + menu)
+
+order = input()
+
+price = 8
+
+quantity = input("How many coffees do you like?\n")
+
+total = price * int(quantity)
+
+print("Thank you. Your Total is: " + str(total))
+
+print("sounds good " + name + ", we'll have that " + order + " ready for you in a moment.")
+
+print("""                                                       
+
+                                             
+                         |    |    |                               
+                         |    |    |                            
+                       _______________                                    
+                      |    Here is    |----\                                        
+                      |      Your     |     |                            
+                      |     coffee    |----/                           
+                       \_____________/                               """)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
